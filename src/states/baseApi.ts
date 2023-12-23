0acc05f775d0d85537e945da1860f5e6b3258b9e
beh_rooz <bmb.mm7@gmail.com>
2023-12-23 21:01:50 +0330

    vite.config.ts and baseApi.ts modify

--- src/states/baseApi.ts
+++ src/states/baseApi.ts
@@ -13,10 +13,6 @@ type Fn = BaseQueryFn<
     >
 
 const baseQuery: Fn = async ({ url, method, data, headers, params, fullResult=false,...moreParams }) => {
-  const userPackageId = localStorage.getItem('user')&&JSON.parse(localStorage.getItem('user'))?.package?.id
-  if(data&&userPackageId){
-    data.package = userPackageId
-  }
   try {
     const result = await axios({ url: import.meta.env.REACT_APP_BASE_URL + url, method, data, params,headers,...moreParams })
     return { data: fullResult?result:result.data }
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),svgr()],
+  plugins: [react(),svgr({include:"**/*.svg"})],
   css:{
     postcss:{
       plugins:[tailwindcss()]
